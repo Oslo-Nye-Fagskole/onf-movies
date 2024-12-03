3de20b6697005af33049c58bf13f17a5eba5c352
Kjell-Magne Øierud <km@oierud.net>
2024-12-03 08:56:03 +0100

    Cleaned up module_6/database.py

--- module_6/database.py
+++ module_6/database.py
@@ -3,39 +3,35 @@ from pprint import pp
 
 con = sqlite3.connect("movie_database.db")
 con.row_factory = sqlite3.Row
+cur = con.cursor()
 
 def movies():
-    cur = con.cursor()
     cur.execute(f'SELECT * FROM movies ORDER BY release_year')
     return cur.fetchall()
 
 def movie(id):
-    cur = con.cursor()
     cur.execute(f'SELECT * FROM movies WHERE id = ?', (id,))
     return cur.fetchone()
 
 def credits(movie_id):
-    cur = con.cursor()
     cur.execute(f'SELECT * FROM credits WHERE movie_id = ?', (movie_id,))
     return cur.fetchall()
 
 def genres():
-    cur = con.cursor()
     cur.execute(f'SELECT * FROM genres')
     return [row[0] for row in cur.fetchall()]
 
+def ppr(rows):
+    pp([dict(r) for r in rows])
 
 def main():
     print("# ONF MOVIES")    
     print("## ALL MOVIES")
-    rows = movies()
-    pp([dict(r) for r in rows])
+    ppr(movies())
     print("## MOVIE #1")
-    rows = movie(1)
-    pp([dict(r) for r in rows])
+    pp(dict(movie(1)))
     print("### CREDITS")
-    rows = credits(1)
-    pp([dict(r) for r in rows])
+    ppr(credits(1))
 
 if __name__ == '__main__':
     main()
